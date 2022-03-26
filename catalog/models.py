db9fd9f31d989ace5a5811058688bb88ff921b4e
PabloLUC9832 <lucaspablo340@outlook.com>
2022-03-25 22:26:25 -0600

    Cambiio

--- catalog/models.py
+++ catalog/models.py
@@ -99,23 +99,19 @@ class BookInstance(models.Model):
         return False
 
 class Author(models.Model):
-    """
-    Modelo que representa un autor
-    """
+    """Model representing an author."""
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
-    date_of_death = models.DateField('Died', null=True, blank=True)
+    date_of_death = models.DateField('died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
 
     def get_absolute_url(self):
-        """
-        Retorna la url para acceder a una instancia particular de un autor.
-        """
+        """Returns the url to access a particular author instance."""
         return reverse('author-detail', args=[str(self.id)])
 
-
     def __str__(self):
-        """
-        String para representar el Objeto Modelo
-        """
-        return '%s, %s' % (self.last_name, self.first_name)
+        """String for representing the Model object."""
+        return '{0}, {1}'.format(self.last_name, self.first_name)
