e573385eb9e0aeaa69e50d4cf4d7a778fb4b7ae4
PabloLUC9832 <lucaspablo340@outlook.com>
2022-03-25 19:52:18 -0600

    Redireccionamientos3

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,7 @@
 from django.urls import include, re_path
 from . import views
 
-urlpatterns = [
+""" urlpatterns = [
     re_path(r'^$', views.index, name='index'),
     #BOOKS
     re_path(r'^books/$', views.BookListView.as_view(), name='books'),
@@ -21,6 +21,41 @@ urlpatterns = [
     re_path('book/create/', views.BookCreate.as_view(), name='book-create'),
     re_path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book-update'),
     re_path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book-delete'),    
+] """
+
+urlpatterns = [
+    re_path('', views.index, name='index'),
+    re_path('books/', views.BookListView.as_view(), name='books'),
+    re_path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    re_path('authors/', views.AuthorListView.as_view(), name='authors'),
+    re_path('author/<int:pk>',
+         views.AuthorDetailView.as_view(), name='author-detail'),
 ]
-   
+
+
+urlpatterns += [
+    re_path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    re_path(r'borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),  # Added for challenge
+]
+
+
+# Add URLConf for librarian to renew a book.
+urlpatterns += [
+    re_path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+
+# Add URLConf to create, update, and delete authors
+urlpatterns += [
+    re_path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    re_path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    re_path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
+]
+
+# Add URLConf to create, update, and delete books
+urlpatterns += [
+    re_path('book/create/', views.BookCreate.as_view(), name='book-create'),
+    re_path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book-update'),
+    re_path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book-delete'),
+]   
 
