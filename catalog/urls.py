51f797656536219f6da586ea52d21d917c35f6aa
PabloLUC9832 <lucaspablo340@outlook.com>
2022-03-25 19:49:24 -0600

    Redireccionamientos1

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
     #Autores
     re_path('authors/', views.AuthorListView.as_view(), name='authors'),
-    re_path('author/<int:pk>',views.AuthorDetailView.as_view(), name='author-detail'),    
+    re_path('^author/<int:pk>',views.AuthorDetailView.as_view(), name='author-detail'),    
     #prestamos
     re_path(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     re_path(r'borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
