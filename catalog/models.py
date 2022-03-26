4ca664df98168ec52c38dffa326ff68981421f80
PabloLUC9832 <lucaspablo340@outlook.com>
2022-03-25 22:42:06 -0600

    cambi0

--- catalog/models.py
+++ catalog/models.py
@@ -62,41 +62,41 @@ class Book(models.Model):
     display_genre.short_description = 'Genre'        
 
 class BookInstance(models.Model):
-    """
-    Modelo que representa una copia específica de un libro (i.e. que puede ser prestado por la biblioteca).
-    """
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="ID único para este libro particular en toda la biblioteca")
-    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this particular book across whole library")
+    book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
-
     borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-
-
-    LOAN_STATUS = (
-        ('m', 'Maintenance'),
-        ('o', 'On loan'),
-        ('a', 'Available'),
-        ('r', 'Reserved'),
-    )
-
-    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Disponibilidad del libro')
-
-    class Meta:
-        ordering = ["due_back"]
-        permissions = (("can_mark_returned", "Set book as returned"),)   
-
-    def __str__(self):
-        """
-        String para representar el Objeto del Modelo
-        """
-        return '%s (%s)' % (self.id,self.book.title)
 
     @property
     def is_overdue(self):
         if self.due_back and date.today() > self.due_back:
             return True
         return False
+
+    LOAN_STATUS = (
+        ('d', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='d',
+        help_text='Book availability')
+
+    class Meta:
+        ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return '{0} ({1})'.format(self.id, self.book.title)
 
 class Author(models.Model):
     """Model representing an author."""
